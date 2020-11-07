2d8f822744ff8dd478fa95ccba2a3d452b96473b
blazkablatnik <blazkablatnik@gmail.com>
2020-11-07 21:48:49 +0100

    correct format

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const { Sequelize, DataTypes, Model, Op } = require('sequelize');
-
+var _ = require('lodash');
 
 
 
@@ -9,47 +9,62 @@ class NewsArticle extends Model { }
 
 
 async function initDb() {
-  const sequelize = new Sequelize('sqlite:../data/news.db')
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+    const sequelize = new Sequelize('sqlite:../data/news.db')
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 
-  /* model definition */
-  NewsArticle.init({
-    title: { type: DataTypes.STRING, allowNull: false },
-    content: { type: DataTypes.STRING, allowNull: true },
-    topic: { type: DataTypes.STRING, allowNull: false },
-    datetime: { type: DataTypes.DATE, allowNull: false },
-    link: { type: DataTypes.STRING, allowNull: false },
-    score: { type: DataTypes.FLOAT },
-  }, {
-    sequelize,
-    timestamps: false,
-    tableName: "news_article"
-  });
+    /* model definition */
+    NewsArticle.init({
+        title: { type: DataTypes.STRING, allowNull: false },
+        content: { type: DataTypes.STRING, allowNull: true },
+        topic: { type: DataTypes.STRING, allowNull: false },
+        datetime: { type: DataTypes.DATE, allowNull: false },
+        link: { type: DataTypes.STRING, allowNull: false },
+        score: { type: DataTypes.FLOAT },
+    }, {
+        sequelize,
+        timestamps: false,
+        tableName: "news_article"
+    });
 
-  /* sync model to db */
-  await sequelize.sync({ force: false });
-  console.log("All models were synchronized successfully.");
+    /* sync model to db */
+    await sequelize.sync({ force: false });
+    console.log("All models were synchronized successfully.");
 
 }
 
-/* DAOOOO */
+/////////
+// DAO
+/////////
 async function findArticlesForTopic(myTopic) {
 
-  const articles = await NewsArticle.findAll({
-    where: {
-      topic: {
-        [Op.eq]: myTopic
-      }
-    }
-  });
+    const articles = await NewsArticle.findAll({
+        where: {
+            topic: {
+                [Op.eq]: myTopic
+            }
+        }
+    });
 
-  console.log("Got " + articles.length + " results for topic " + myTopic + ".");
-  return articles;
+    // Group articles by day
+    const articlesByDate = _.groupBy(articles, (article) =>
+        `${article.datetime.getDate()}/${article.datetime.getMonth()}/${article.datetime.getFullYear()}`
+    )
+
+    // Sort dates
+    const sortedDates = Object.keys(articlesByDate).sort()
+
+    // Nest articles under corresponding date
+    return _.map(sortedDates, (date) => {
+        return {
+            datetime: date,
+            news: _.sortBy(articlesByDate[date], (article) => article.datetime)
+        }
+    })
 }
 
 
@@ -58,28 +73,28 @@ async function findArticlesForTopic(myTopic) {
 /* public static void main(String[] args) */
 (async () => {
 
-  await initDb();
+    await initDb();
 
-  /* server */
-  const app = express()
-  const port = 8080
+    /* server */
+    const app = express()
+    const port = 8080
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-  });
-
-  app.get('/timeline/:topic', async (req, res) => {
-    const topic = req.params.topic;
-
-    const articles = await findArticlesForTopic(topic);
-    res.json({
-      "topic": topic,
-      "dates": articles
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname + '/index.html'));
     });
-  });
 
-  app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
-  });
+    app.get('/timeline/:topic', async (req, res) => {
+        const topic = req.params.topic;
+
+        const articles = await findArticlesForTopic(topic);
+        res.json({
+            "topic": topic,
+            "dates": articles
+        });
+    });
+
+    app.listen(port, () => {
+        console.log(`Server listening at http://localhost:${port}`)
+    });
 
 })();
